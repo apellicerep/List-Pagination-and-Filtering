eafadb9b81687b10c6f04047737a3e1135e9db0e
apellicerep <42895016+apellicerep@users.noreply.github.com>
2019-09-05 12:06:43 +0200

    Refractoring code

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@
 List Filter and Pagination --
 ******************************************/
 
-
-// Add variables that store DOM elements I will need to reference and/or manipulate
-
 const listUsers = document.querySelectorAll(".student-item");//select all students and I put them in Array.
+const numUsers=listUsers.length;
 const usersPerPage = 10;
 const pages= Math.ceil(listUsers.length/usersPerPage); //number of pages.
 const page = document.querySelector(".page");
+//Create div pagination
+const divPagination = document.createElement("div")
+divPagination.className="pagination"
+const ulPagination = document.createElement("ul")
+divPagination.appendChild(ulPagination)
+page.appendChild(divPagination)
 
 
 //SEARCHBOX
@@ -22,212 +26,77 @@ divSearch.appendChild(input);
 divSearch.appendChild(button);
 page.firstElementChild.appendChild(divSearch);
 
-//PAGINATION
-const divPages = document.createElement("div")
-//append <ul> in div
-divPages.appendChild(createPaginationElements(pages));
-divPages.className = "pagination";
-//append <div class = pagination> to <div class= page>
-page.appendChild(divPages);
 
-//BUTTONBACK
-const buttonBack = document.createElement("button");
-buttonBack.textContent="Back";
-buttonBack.className="back";
+
+//initilize the number of page buttons
+printListPagination(pages)
+
+//Starts showing first page users
+showUsers(1)
 
 
 
-// Create and append the pagination links, and returns <ul> with the correct number of li depending
-//on how many Students and Students per Page.
-function createPaginationElements(pages){
-  //let backButton = argument[2];
-  let liPages=[];
-  //create elements.
-  function createElement(tag){
-    let element = document.createElement(tag);
-    return element;
+//Logic for displaying the Users
+function showUsers(num){
+  let max=num*10;
+  let min=max-10;
+  if(num==pages) max=numUsers;
+  for(let i of listUsers){
+    i.style.display="none"
   }
+  for(let i=min;i<max;i++){
+    listUsers[i].style.display="block"
+  }
+}
 
-  //create Array of <il> with its <a>
-  for(let i=0; i<pages;i++){
-    let item = document.createElement("li");
-    let anchor = document.createElement("a");
+//Function that prints the pageButtons
+function printListPagination(pages){
+  let pageButton = ""
+  for(let i=0;i<pages;i++){
     if(i==0){
-      anchor.className="active";//when page is load the first nº page is active.
-    }
-    anchor.href = "#";
-    anchor.textContent = i+1;//nº page starts from 1 not from 0.
-    item.append(anchor);
-    liPages.push(item);
-  }
-  //Create <ul>
-  let ulPages = document.createElement("ul");
-
-  //append li in <ul>
-  for(let i=0;i<liPages.length;i++){
-    ulPages.appendChild(liPages[i]);
-  }
-  return ulPages;
-}
-
-
-//Cleans the value in the input.
-function cleanInput(){
-  let input = document.querySelector("input");
-  input.value = "";
-}
-
-//active or inactive the pagination anchor.
-function activate1stPage(){
-  let active = document.querySelector(".active");
-  let inactiveToActive = document.querySelector("a");
-  active.className="inactive";
-  inactiveToActive.className="active";
-
-
-}
-
-
-//function receive nº of pages and creates an Array of "1"(show) and "0".(hide).
-//Calculates the limits of Students to show depending of the page clicked.
-//Example: if page is  1 => [1,1,1,1,1,1,1,1,1,1,0,0,0,0,.....]
-function arrayIndexStudentsToShow2(page){
-  let arrayIndexStudentToShow = [];
-  for (let i=0;i<listUsers.length;i++){
-    arrayIndexStudentToShow.push("0");
-  }
-  let limitBottom=(usersPerPage*page)-usersPerPage;//Example with nº page =1 =>(10*1)-10 = 0;
-  let limitTop=(usersPerPage*page);//Example with nº page =1=> (10*1)-1= 9;
-  for(limitBottom;limitBottom<limitTop;limitBottom++){
-    for(let i=0;i<arrayIndexStudentToShow.length;i++){
-      if(i==(limitBottom)){
-        arrayIndexStudentToShow[i] ="1";
-      }
-  }
-}
-  return arrayIndexStudentToShow;
-}
-
-//receive and array of words from Input and search/match in listUsers if that user exists.
-//then returns an Array with the index of the users that matched.
-function searchStudent(arrayWords){
-  console.log(arrayWords);
-  let ok;
-  let arrayStudentsMostrar=[];
-  let arrayIndexStudentsSearch=[];
-  for(let i=0;i<listUsers.length;i++){
-    ok=0;
-    let h3 = listUsers[i].firstElementChild.firstElementChild.nextElementSibling;
-    let name = h3.textContent;
-    let nameSplit = name.split("");
-    let span = h3.nextElementSibling;
-    let email = span.textContent;
-      for(let i=0;i<arrayWords.length;i++){
-          if(arrayWords[i]===nameSplit[i]){
-            ok++;
-          }
-        }
-    if(ok===arrayWords.length){
-      arrayIndexStudentsSearch.push(i);
-      console.log(listUsers[i]);
-      }
-  }
-  return arrayIndexStudentsSearch; //Array with index of students to show.
-}
-
-//recevie and array with index of users that matches and creates and array of "0" and "1".
-//Example: arrayIndexStudentsSearch = [2,4] then ==> arrayIndexStudentToShow =[0,1,0,1,0,0,0,....]
-function arrayIndexStudentsSearch(arrayIndexStudentsSearch){
-  console.log(arrayIndexStudentsSearch);
-  let arrayIndexStudentToShow = [];
-  for (let i=0;i<listUsers.length;i++){ //initialize array with all "0"
-    arrayIndexStudentToShow.push("0");
-  }
-  for(let i=0;i<arrayIndexStudentsSearch.length;i++){
-    for(let j=0;j<listUsers.length;j++){
-      if(arrayIndexStudentsSearch[i]===j){
-        arrayIndexStudentToShow[j]="1";
-      }
-
+      pageButton += `<li><a class="active">${i+1}</a></li>`
+    }else{
+       pageButton += `<li><a>${i+1}</a></li>`
     }
   }
-  console.log(arrayIndexStudentToShow);
-  return arrayIndexStudentToShow;
+  ulPagination.innerHTML= pageButton
 }
 
 
-//function receive Array of Students that contains "1" and "0". if "1" display user, else block.
-function hideShowStudents(arrayIndexStudentToShow){
-  let count =0;
-  let pagination = document.querySelector(".pagination");
-  console.log(arrayIndexStudentToShow);
-    for(let i=0; i<arrayIndexStudentToShow.length;i++){
-        if(arrayIndexStudentToShow[i]==="1"){
-          listUsers[i].style.display="block";
-          buttonBack.style.display="block";
-
-
-      }else{
-        listUsers[i].style.display="none";
-        count++;
-        buttonBack.style.display="block";
-      }
-
-      }
-
-      if(count ===listUsers.length){
-        pagination.style.display ="none";
-        page.appendChild(buttonBack);
-        buttonBack.style.display="block";
-      }
-      else{
-        buttonBack.style.display="none";
-
-      }
+//Add EventListener to de Input
+input.addEventListener("keyup",(event)=>{
+  let value = event.target.value
+  let studentDetails = document.querySelectorAll(".student-details > h3")
+  console.log(value)
+  console.log(value)
+  for(let i of studentDetails){
+    i.parentNode.parentNode.style.display="none"
+    let nombre = i.textContent
+   if(nombre.includes(value)){
+     i.parentNode.parentNode.style.display="block"
+   }
   }
-
-//initialize in page 1 when html and css is Loaded.
-hideShowStudents(arrayIndexStudentsToShow2(1));
-
-/*When user clics the page number it calculates the limits of a range of numbers.then it will pass that limits
-to hide/show the elements.*/
-
-const pagination = document.querySelector(".pagination");
-pagination.addEventListener("click",(e)=>{
-  let page = e.target.textContent;
-  hideShowStudents(arrayIndexStudentsToShow2(page));
-  if(e.target.tagName ==="A"){
-    let anchor = document.querySelector(".active");
-    anchor.className="inactive";
-    e.target.className ="active";
-    }
-})
-
-
-//EventListener in Input, when user keyup I get value
-input.addEventListener("keyup",(e)=>{
-  let pagination = document.querySelector(".pagination");
-  pagination.style.display="none";
-  let arrayWords =[];
-  let word = input.value.toLowerCase();
-  console.log(word);
-  word.split("");
-  if(word===""){
-    hideShowStudents(arrayIndexStudentsToShow2(1));
-    pagination.style.display="block";
-    activate1stPage();
-
+  if(value===""){
+    showUsers(1)
+    printListPagination(pages)
   }else{
-
-  hideShowStudents(arrayIndexStudentsSearch(searchStudent(word)));
-}
+    printListPagination(0)
+  }
 })
 
-//eventListener in Button Back.
-buttonBack.addEventListener("click",(e)=>{
-  hideShowStudents(arrayIndexStudentsToShow2(1));
-  pagination.style.display="block";
-  cleanInput();
-  activate1stPage();
 
+//Add EventListener to the pagination Button
+ulPagination.addEventListener("click",(event)=>{
+  console.log(event.target.tagName)
+  let anchors=document.querySelectorAll("a")
+  if(event.target.tagName=="A"){
+    let valuePag =event.target.textContent
+    showUsers(valuePag)
+    for(let i of anchors){
+      i.className="";
+    }
+    event.target.className="active"
+    
+  } 
+  
 })
